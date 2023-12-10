5b4a255ab7128ad824746e5c80e0c3e46d7750d6
Rich Siomporas <richietommy@yahoo.com>
2023-12-10 12:20:09 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 KERNEL_VERSION="${KERNEL_VERSION:-$(uname -r)}"
-NVIDIA_DRIVER_VERSION="${NVIDIA_DRIVER_VERSION:-535.104.12}"
+NVIDIA_DRIVER_VERSION="${NVIDIA_DRIVER_VERSION:-535.104.05}"
 CONTAINER_REGISTRY="${CONTAINER_REGISTRY:-container-registry.siomporas.com}"
 RPM_BASE_URL="${RPM_BASE_URL:-https://dl.rockylinux.org/pub/rocky/8/BaseOS/x86_64/os/Packages}"
 OS_NAME="${OS_NAME:-$(cat /etc/os-release | grep ^ID= | awk -F= '{print $2}' | sed -e 's/^"//' -e 's/"$//')}"
