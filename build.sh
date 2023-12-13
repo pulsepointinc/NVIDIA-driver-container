171891a7df4f899d6ac60a367733a4237c94c83d
Rich Siomporas <richietommy@yahoo.com>
2023-12-12 20:51:56 -0500

    Update build.sh
    Added TODO after EL8.9 release broke things

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cd $SCRIPT_DIR
 KERNEL_VERSION="${KERNEL_VERSION:-$(uname -r)}"
 NVIDIA_DRIVER_VERSION="${NVIDIA_DRIVER_VERSION:-535.104.05}"
 CONTAINER_REGISTRY="${CONTAINER_REGISTRY:-container-registry.siomporas.com}"
+# TODO - pin OS releases to their vault URLs like https://dl.rockylinux.org/vault/rocky/8.8, maybe parameterize packages in install script too
 RPM_BASE_URL="${RPM_BASE_URL:-https://dl.rockylinux.org/pub/rocky/8/BaseOS/x86_64/os/Packages}"
 OS_NAME="${OS_NAME:-$(cat /etc/os-release | grep ^ID= | awk -F= '{print $2}' | sed -e 's/^"//' -e 's/"$//')}"
 OS_RELEASE="${OS_RELEASE:-$(cat /etc/os-release | grep VERSION_ID | awk -F= '{print $2}' | sed -e 's/^"//' -e 's/"$//')}"
