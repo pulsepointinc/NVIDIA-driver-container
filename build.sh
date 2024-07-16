5e1f5bfc2710d1340adaad41bc7a9e480e2269fe
Jakub Drahos <10064042+jdrahos@users.noreply.github.com>
2024-07-16 11:28:42 -0400

    meaningful defaults for build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 KERNEL_VERSION="${KERNEL_VERSION:-$(uname -r)}"
-NVIDIA_DRIVER_VERSION="${NVIDIA_DRIVER_VERSION:-535.129.03}"
-CONTAINER_REGISTRY="${CONTAINER_REGISTRY:-container-registry.siomporas.com}"
+NVIDIA_DRIVER_VERSION="${NVIDIA_DRIVER_VERSION:-550.54.14}"
+CONTAINER_REGISTRY="${CONTAINER_REGISTRY:-registry.pulsepoint.com}"
 # TODO - pin OS releases to their vault URLs like https://dl.rockylinux.org/vault/rocky/8.8, maybe parameterize packages in install script too
 RPM_BASE_URL="${RPM_BASE_URL:-https://dl.rockylinux.org/pub/rocky/8/BaseOS/x86_64/os/Packages}"
 OS_NAME="${OS_NAME:-$(cat /etc/os-release | grep ^ID= | awk -F= '{print $2}' | sed -e 's/^"//' -e 's/"$//')}"
